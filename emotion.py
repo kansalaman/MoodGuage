d6a1c541a48e6043b5d6ad6392c13aedac44d14f
kansalaman <amankansal.india@gmail.com>
2019-09-29 15:18:48 +0530

    switch to speech

--- emotion.py
+++ emotion.py
@@ -20,7 +20,7 @@ def feature_extract(wav_rd):
 	#print(sig)
 	mfcc_feat = mfcc(sig,rate,nfft=1200)
 	fbank_feat = logfbank(sig,rate,nfft=1200)
-	return fbank_feat.flatten('F')[:10998]
+	return fbank_feat.flatten('F')[:8554]
 
 def feature_select(X,y,k=400):
 	#ANOVA-F
@@ -47,23 +47,23 @@ if __name__=='__main__':
 
 	#print(fbank_feat.shape)
 	rd_file = [[] for _ in range(11)]
-	X=np.zeros([11,10998])
+	X=np.zeros([11,8554])
 	y=[1,2,3,1,2,3,1,2,3,1,3]
 
-	rd_file[0] = read("Audio_Song_Actors_01-24/Actor_01/03-02-01-01-01-01-01.wav")
-	rd_file[1] = read("Audio_Song_Actors_01-24/Actor_01/03-02-02-01-01-01-01.wav")
-	rd_file[2] = read("Audio_Song_Actors_01-24/Actor_01/03-02-03-01-01-01-01.wav")
-	rd_file[3] = read("Audio_Song_Actors_01-24/Actor_02/03-02-01-01-01-01-02.wav")
-	rd_file[4] = read("Audio_Song_Actors_01-24/Actor_01/03-02-02-01-01-01-01.wav")
-	rd_file[5] = read("Audio_Song_Actors_01-24/Actor_02/03-02-03-01-01-01-02.wav")
-	rd_file[6] = read("Audio_Song_Actors_01-24/Actor_01/03-02-01-01-01-01-01.wav")
-	rd_file[7] = read("Audio_Song_Actors_01-24/Actor_01/03-02-02-01-01-01-01.wav")
-	rd_file[8] = read("Audio_Song_Actors_01-24/Actor_01/03-02-03-01-01-01-01.wav")
-	rd_file[9] = read("Audio_Song_Actors_01-24/Actor_04/03-02-01-01-01-01-04.wav")
-	#X[10] = feature_extract("Audio_Song_Actors_01-24/Actor_04/03-02-02-01-01-01-04.wav").flatten('F')[:10998]
-	rd_file[10] = read("Audio_Song_Actors_01-24/Actor_01/03-02-03-01-01-01-01.wav")
+	rd_file[0] = read("Audio_Speech_Actors_01-24/Actor_01/03-01-01-01-01-01-01.wav")
+	rd_file[1] = read("Audio_Speech_Actors_01-24/Actor_01/03-01-02-01-01-01-01.wav")
+	rd_file[2] = read("Audio_Speech_Actors_01-24/Actor_01/03-01-03-01-01-01-01.wav")
+	rd_file[3] = read("Audio_Speech_Actors_01-24/Actor_02/03-01-01-01-01-01-02.wav")
+	rd_file[4] = read("Audio_Speech_Actors_01-24/Actor_02/03-01-02-01-01-01-01.wav")
+	rd_file[5] = read("Audio_Speech_Actors_01-24/Actor_02/03-01-03-01-01-01-02.wav")
+	rd_file[6] = read("Audio_Speech_Actors_01-24/Actor_01/03-01-01-01-01-01-01.wav")
+	rd_file[7] = read("Audio_Speech_Actors_01-24/Actor_01/03-01-02-01-01-01-01.wav")
+	rd_file[8] = read("Audio_Speech_Actors_01-24/Actor_01/03-01-03-01-01-01-01.wav")
+	rd_file[9] = read("Audio_Speech_Actors_01-24/Actor_04/03-01-01-01-01-01-04.wav")
+	#X[10] = feature_extract("Audio_Speech_Actors_01-24/Actor_04/03-02-02-01-01-01-04.wav").flatten('F')[:10998]
+	rd_file[10] = read("Audio_Speech_Actors_01-24/Actor_01/03-01-03-01-01-01-01.wav")
 
-	Z = feature_extract(preprocess(read("Audio_Song_Actors_01-24/Actor_04/03-02-02-01-01-01-04.wav")))
+	Z = feature_extract(preprocess(read("Audio_Speech_Actors_01-24/Actor_04/03-01-02-01-01-01-04.wav")))
 
 	for i in range(len(rd_file)):
 		rd_file[i]=preprocess(rd_file[i])

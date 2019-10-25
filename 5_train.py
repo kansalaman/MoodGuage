b67d0aec46fcecd039572ad54821c319888249d7
Arnabjana1999 <ajana47@gmail.com>
2019-10-25 22:50:31 +0530

    ML models added

--- 5_train.py
+++ 5_train.py
@@ -18,7 +18,7 @@ import itertools
 from IPython.display import display
 from utils import *
 
-get_ipython().magic('matplotlib inline')
+#get_ipython().magic('matplotlib inline')
 
 if __name__ == '__main__':
 
@@ -27,32 +27,45 @@ if __name__ == '__main__':
 	
 	emotion_dict = {'neutral': 1, 'calm': 2, 'happy': 3, 'sad': 4, 'angry': 5, 'fearful': 6, 'disgust': 7, 'surprised': 8,}
 	emo_keys = list(['neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgust', 'surprised'])
+
+	# emotion_dict = {'happy': 0, 'sad': 1, 'angry' : 2, 'surprised' : 3}
+	# emo_keys = list(['happy', 'sad', 'angry', 'surprised'])
 	raw_x_train, y_train, raw_x_test, y_test = read_raw_data_from_csv(train_csv_file, test_csv_file)
+	#print(y_train[:10])
 	print("Reading done")
-	x_train, x_test = processed_data_from_raw(x_train, x_test)
+	x_train, x_test = processed_data_from_raw(raw_x_train, raw_x_test)
 	print("Processing done")
+	# print(x_train)
+	# print(y_train)
+	# l=1/0
 
-	rf_classifier = RandomForestClassifier(n_estimators=1200, min_samples_split=25)
-	ml_model(rf_classifier, x_train, y_train, x_test, y_test, emo_keys, model_name='rf_classifier')
+	# rf_classifier = RandomForestClassifier(n_estimators=2000, min_samples_split=45)
+	# ml_model(rf_classifier, x_train, y_train, x_test, y_test, emotion_dict, emo_keys, model_name='rf_classifier')
+	# print("Random Forest done")
 	xgb_classifier = xgb.XGBClassifier(max_depth=7, learning_rate=0.008, objective='multi:softprob', 
-                                   n_estimators=1200, sub_sample=0.8, num_class=len(emotion_dict),booster='gbtree', n_jobs=4)
-	ml_model(xgb_classifier, x_train, y_train, x_test, y_test, emo_keys, model_name='xgb_classifier')
-	mlp_classifier = MLPClassifier(hidden_layer_sizes=(650, ), activation='relu', solver='adam', alpha=0.0001,
-                               batch_size='auto', learning_rate='adaptive', learning_rate_init=0.01,
-                               power_t=0.5, max_iter=1000, shuffle=True, random_state=None, tol=0.0001,
-                               verbose=False, warm_start=True, momentum=0.8, nesterovs_momentum=True,
-                               early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999,
-                               epsilon=1e-08)
-	ml_model(mlp_classifier, x_train, y_train, x_test, y_test, emo_keys, model_name='mlp_classifier')
-	svc_classifier = LinearSVC()
-	ml_model(svc_classifier, x_train, y_train, x_test, y_test, emo_keys, model_name='svc_classifier')
-	mnb_classifier = MultinomialNB()
-	ml_model(mnb_classifier, x_train, y_train, x_test, y_test, emo-keys, model_name='mnb_classifier')
-	lr_classifier = LogisticRegression(solver='lbfgs', multi_class='multinomial', max_iter=1000)
-	ml_model(lr_classifier, x_train, y_train, x_test, y_test, emo_keys, model_name='lr_classifier')
+                                   n_estimators=2000, sub_sample=0.5, num_class=len(emotion_dict), booster='gbtree', n_jobs=4)
+	ml_model(xgb_classifier, x_train, y_train, x_test, y_test, emotion_dict, emo_keys, model_name='xgb_classifier')
+	print("XGB done")
+	# mlp_classifier = MLPClassifier(hidden_layer_sizes=(650, ), activation='relu', solver='adam',
+ #                               batch_size='auto', learning_rate='adaptive', learning_rate_init=0.0001,
+ #                               power_t=0.5, max_iter=1000, shuffle=True, random_state=None, tol=0.0001,
+ #                               verbose=False, warm_start=False, momentum=0.8, nesterovs_momentum=True,
+ #                               early_stopping=False, validation_fraction=0.05, beta_1=0.9, beta_2=0.999,
+ #                               epsilon=1e-08)
+	# ml_model(mlp_classifier, x_train, y_train, x_test, y_test, emotion_dict, emo_keys, model_name='mlp_classifier')
+	# print("MLP done")
+	# svc_classifier = LinearSVC(tol=0.0001, max_iter=1000)
+	# ml_model(svc_classifier, x_train, y_train, x_test, y_test, emotion_dict, emo_keys, model_name='svc_classifier', svm=True)
+	# print("SVC done")
+	# mnb_classifier = MultinomialNB()
+	# ml_model(mnb_classifier, x_train, y_train, x_test, y_test, emotion_dict, emo_keys, model_name='mnb_classifier')
+	# print("MNB done")
+	# lr_classifier = LogisticRegression(solver='lbfgs', multi_class='multinomial', max_iter=1000)
+	# ml_model(lr_classifier, x_train, y_train, x_test, y_test, emotion_dict, emo_keys, model_name='lr_classifier')
+	# print("LR done")
 
-	display_xgb_feature_importance(xgb_classifier)
-	xgb_evaluation_on_reduced_feature_set(xgb_classifier, emo_keys)
+	# display_xgb_feature_importance(xgb_classifier)
+	# xgb_evaluation_on_reduced_feature_set(x_train, y_train, x_test, y_test, xgb_classifier, emotion_dict, emo_keys)
 
-	perform_ensembles(y_test, [0,1,4])
+	# perform_ensembles(y_test, [0,1,4], emotion_dict, emo_keys)
 
--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ from sklearn.utils.class_weight import compute_class_weight
 import itertools
 from IPython.display import display
 
-get_ipython().magic('matplotlib inline')
+#get_ipython().magic('matplotlib inline')
 
 def read_raw_data_from_csv(train_csv_file, test_csv_file):
 	x_train = pd.read_csv(train_csv_file)
@@ -29,13 +29,19 @@ def read_raw_data_from_csv(train_csv_file, test_csv_file):
 
 def processed_data_from_raw(x_train, x_test):
 	#print(x_train.shape, x_test.shape)
+	#print("Doing1")
 	cl_weight = dict(pd.Series(x_train['label']).value_counts(normalize=True))
+	#print("Done1")
 	#print(dict(pd.Series(x_train['label']).value_counts()))
 
 	del x_train['label']
+	#print("Done2")
 	del x_test['label']
-	del x_train['wav_file']
-	del x_test['wav_file']
+	#print("Done3")
+	del x_train['file_name']
+	del x_test['file_name']
+	del x_train['Unnamed: 0']
+	del x_test['Unnamed: 0']
 	return x_train, x_test
 
 def plot_confusion_matrix(cm, classes,normalize=False,title='Confusion matrix',cmap=plt.cm.Blues):
@@ -63,25 +69,44 @@ def plot_confusion_matrix(cm, classes,normalize=False,title='Confusion matrix',c
 	plt.xlabel('Predicted label')
     
 def one_hot_encoder(true_labels, num_records, num_classes):
-    temp = np.array(true_labels[:num_records])
-    true_labels = np.zeros((num_records, num_classes))
-    true_labels[np.arange(num_records), temp] = 1
-    return true_labels
+	#print(true_labels)
+	temp = np.array(true_labels[:num_records])
+	true_labels = np.zeros((num_records, num_classes))
+	true_labels[np.arange(num_records), temp] = 1
+	#print(true_labels)
+	#print("-----")
+	return true_labels
 
-def show_evaluation_metrics(y_test, pred_probs, emo_keys, cm=True):
+def show_evaluation_metrics(y_test, pred_probs, emotion_dict, emo_keys, cm=True):
 	pred = np.argmax(pred_probs, axis=-1)
+	#print(pred)
+	'''for i in range(50):
+		print(y_test[i], pred[i])
+	print("-----")'''
 	one_hot_true = one_hot_encoder(y_test, len(pred), len(emotion_dict))
 	print('Test Set Accuracy =  {0:.3f}'.format(accuracy_score(y_test, pred)))
 	print('Test Set F-score =  {0:.3f}'.format(f1_score(y_test, pred, average='macro')))
 	print('Test Set Precision =  {0:.3f}'.format(precision_score(y_test, pred, average='macro')))
 	print('Test Set Recall =  {0:.3f}'.format(recall_score(y_test, pred, average='macro')))
-	if cm:
-		plot_confusion_matrix(confusion_matrix(y_test, pred), classes=emo_keys)
+	print(confusion_matrix(y_test, pred))
+	#if cm:
+	#	plot_confusion_matrix(confusion_matrix(y_test, pred), classes=emo_keys)
 
-def ml_model(classifier, x_train, y_train, x_test, y_test, emo_keys, model_name, dump=True):
+def ml_model(classifier, x_train, y_train, x_test, y_test, emotion_dict, emo_keys, model_name, svm=False, dump=True):
+	#print(x_train.shape)
+	#print(y_train[:20])
 	classifier.fit(x_train, y_train)
-	predictions = rf_classifier.predict_proba(x_test)
-	show_evaluation_metrics(y_test, predictions, emo_keys)
+	if svm:
+		#print("Here")
+		predictions = classifier.predict(x_test)
+		predictions = one_hot_encoder(predictions, len(predictions), len(emotion_dict))
+	else:
+		#print('here I am')	
+		predictions = classifier.predict_proba(x_test)
+		#print(predictions)
+	#print(x_test.shape)
+	#print(predictions.shape)
+	show_evaluation_metrics(y_test, predictions, emotion_dict, emo_keys)
 	if dump:
 		with open('pred_probas/{}.pkl'.format(model_name), 'wb') as f:
 			pickle.dump(predictions, f)
@@ -91,7 +116,7 @@ def display_xgb_feature_importance(xgb_classifier):
 	fig = ax.figure
 	fig.set_size_inches(8, 8)
 
-def xgb_evaluation_on_reduced_feature_set(xgb_classifier, emo_keys):
+def xgb_evaluation_on_reduced_feature_set(x_train, y_train, x_test, y_test, xgb_classifier, emotion_dict, emo_keys):
 	contribution_scores = xgb_classifier.feature_importances_
 	top_n = [3, 5, 6, 7, 8]
 	for n in top_n:
@@ -103,13 +128,14 @@ def xgb_evaluation_on_reduced_feature_set(xgb_classifier, emo_keys):
 		select_xgb_classifier = xgb.XGBClassifier(max_depth=7, learning_rate=0.008, objective='multi:softprob', 
                                               n_estimators=1200, sub_sample = 0.8, num_class = len(emotion_dict),
                                               booster='gbtree', n_jobs=4)
-		ml_model(select_xgb_classifier, select_x_train, y_train, select_x_test, y_test, emo_keys, model_name='garbage', dump=False)
+		ml_model(select_xgb_classifier, select_x_train, y_train, select_x_test, y_test, emotion_dict, emo_keys, model_name='garbage', dump=False)
 		'''select_xgb_classifier.fit(select_x_train, y_train)
 		predictions = select_xgb_classifier.predict_proba(select_x_test)
 		show_evaluation_metrics(y_test, predictions, cm = False)'''
 
-def perform_ensembles(y_test, models, emo_keys):
+def perform_ensembles(y_test, models, emotion_dict, emo_keys):
 	x = [0 for _ in range(7)]
+	ensemble_pred_probs = 0.0
 
 	with open('pred_probas/rf_classifier.pkl', 'rb') as f:
 		x[0] = pickle.load(f)
@@ -129,18 +155,18 @@ def perform_ensembles(y_test, models, emo_keys):
 	with open('pred_probas/lr_classifier.pkl', 'rb') as f:
 		x[5] = pickle.load(f)
 
-	with open('pred_probas/lstm_classifier.pkl', 'rb') as f:
-		x[6] = pickle.load(f)
+	'''with open('pred_probas/lstm_classifier.pkl', 'rb') as f:
+		x[6] = pickle.load(f)'''
 
 	# Average of the predicted probabilites
 	for i in models:
 		ensemble_pred_probs+=x[i]
 	ensemble_pred_probs/=len(models)
 	# Show metrics
-	show_evaluation_metrics(y_test, ensemble_pred_probs, emo_keys)
+	show_evaluation_metrics(y_test, ensemble_pred_probs, emotion_dict, emo_keys)
 
 def up_sample(df,label):
     emo_df = df[df['label']==label]
-    for i in range(20):
+    for i in range(10):
         df = df.append(emo_df)
     return df
